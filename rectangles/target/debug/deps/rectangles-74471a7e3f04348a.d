2452bdbc72bce4bb306294c11dda6852d26c82d1
Victor Yip <victorgyip@gmail.com>
2023-01-01 02:15:02 -0500

    commit2

---
+++ rectangles/target/debug/deps/rectangles-74471a7e3f04348a.d
@@ -0,0 +1,5 @@
+/home/victor/rustrepo1/rust_repo1/rectangles/target/debug/deps/rectangles-74471a7e3f04348a: src/main.rs
+
+/home/victor/rustrepo1/rust_repo1/rectangles/target/debug/deps/rectangles-74471a7e3f04348a.d: src/main.rs
+
+src/main.rs:
---
+++ rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+/home/victor/rustrepo1/rust_repo1/rectangles/target/debug/rectangles: /home/victor/rustrepo1/rust_repo1/rectangles/src/main.rs
